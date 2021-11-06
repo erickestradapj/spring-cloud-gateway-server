c685cccaf1a5e513f74d26a7873ad069e08d1a7f
Erick Estrada <erickestradapj@gmail.com>
2021-11-05 22:07:38 -0500

    starting project

---
+++ src/test/java/com/dev/spring/app/gateway/SpringServiceGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.spring.app.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServiceGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
